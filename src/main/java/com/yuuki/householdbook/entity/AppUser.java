6d688c41b6c564e0c50c8be15ca1750e8750070b
whitecat1216 <yuukikotani1216@gmail.com>
2025-10-10 00:41:08 +0900

    [add]ユーザーにメールアドレスの入力欄を実装

--- src/main/java/com/yuuki/householdbook/entity/AppUser.java
+++ src/main/java/com/yuuki/householdbook/entity/AppUser.java
@@ -18,6 +18,12 @@ public class AppUser {
 
     private String role; // 例: "USER", "ADMIN"
 
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
     // --- Getter & Setter ---
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
